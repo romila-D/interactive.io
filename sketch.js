e0ff3fccfadb8fc9258de2589ef7f6a2cf4098e0
romila-D <romila.faheem@gmail.com>
2025-05-30 15:26:22 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,47 +1,37 @@
-let quote = "Believe in yourself.";
-let words = quote.split(" ");
-let fontSizes = [24, 36, 48, 60];
-let fontColors = ["#67630A", "#9C27B0", "#009688"];
-let angles = [0, 0, 0, 0];
-let velocities = [0.5, 1, 1.5, 2];
-let yPositions = [200, 300, 400, 500];
 
 function setup() {
-  createCanvas(1000, 1000);
-  background(255);
-  textSize(24);
-  textAlign(CENTER, CENTER);
+  createCanvas(600, 400);
+  background(220);
 }
 
 function draw() {
-  background(173, 216 , 230);
+  background(220);
   
-  fill(0);
-  textSize(32);
-  text("Click On The Words", width / 2, 50);
+  // Face
+  fill(0, 255, 0); // Green
+  ellipse(300, 170, 100, 150);
   
+  // Body
+  fill(0, 255, 0); // Green
+  ellipse(300, 300, 150, 200);
   
-  for (let i = 0; i < words.length; i++) {
-    fill(fontColors[i % fontColors.length]);
-    textSize(fontSizes[i % fontSizes.length]);
-    push();
-    translate(width / 2, yPositions[i]);
-    rotate(radians(angles[i]));
-    text(words[i], 0, 0);
-    pop();
-    
-    angles[i] += velocities[i];
-    
-    if (angles[i] > 360) {
-      angles[i] -= 360;
-    }
-  }
-}
-
-function mousePressed() {
-  for (let i = 0; i < words.length; i++) {
-    if (dist(mouseX, mouseY, width / 2, yPositions[i]) < 50) {
-      velocities[i] *= -1;
-    }
-  }
+  // Eyes
+  fill(255); // big
+  ellipse(275, 135, 25, 25);
+  ellipse(325, 135, 25, 25);
+  fill(0); // small
+  ellipse(275, 135, 10, 10);
+  ellipse(325, 135, 10, 10);
+  
+  // lips
+  fill(0); // Black
+  arc(300, 175, 50, 25, 0, PI);
+  
+  // Antina
+  stroke(0); // Black
+  strokeWeight(5);
+  line(300, 130, 300, 100);
+  strokeWeight(2);
+  line(300, 100, 250, 50);
+  line(300, 100, 350, 50);
 }
