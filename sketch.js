b0174dd5546d84663ab544512237d271d21864d2
romila-D <romila.faheem@gmail.com>
2025-05-30 15:31:19 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,41 +1,33 @@
-// stick man's position
-let x = 0;
-let y = 250;
-
-// stick man's size
-let w = 50;
-let h = 100;
-
-// stick man's speed
-let speed = 3;
-
 function setup() {
   createCanvas(600, 400);
-  frameRate(30);
+  background(220);
 }
 
 function draw() {
   background(220);
-
-  // draw stick man
-  stroke(0); // Black
-  strokeWeight(5);
-
-  // stick man head
-  fill(255,0,0);
-  ellipse(x + w/2, y, 30, 30);
-
-  // stick man body
-  line(x + w/2,y + 15,x + w/2,y + h - 15);
-
-  // stick man arms
-  line(x + w/2,y + 30,x + w/2 - 20,y + 50);
-  line(x + w/2,y + 30,x + w/2 + 20,y + 50);
-
-  // stick man legs
-  line(x + w/2,y + h - 15,x + w/2 - 20,y + h + 30);
-  line(x + w/2,y + h - 15,x + w/2 + 20,y + h + 30);
-
-  // move the stick man
-  x += speed;
+  
+  // Wheels
+  fill(0); // Black
+  ellipse(280, 300, 50, 50);
+  ellipse(370, 300, 50, 50);
+  
+   
+  
+  // Car body
+  fill(255, 0, 0); // Red
+  rect(250, 200, 150, 100);
+  
+  // Windows
+  fill(135, 206, 235); // Sky blue
+  rect(260, 210, 60, 50);
+  fill(135, 206, 235); // Sky blue
+  rect(330, 210, 60, 50);
+  
+  
+ 
+  
+  // Door handles
+  fill(0); // Black
+  rect(385, 270, 5, 20);
+  rect(315, 270, 5, 20);
 }
