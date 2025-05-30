b84e610acaacdc8feab3b1264c876c0127295deb
romila-D <romila.faheem@gmail.com>
2025-05-30 15:29:31 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,23 +12,28 @@ function setup() {
 
 function draw() {
   background(0);
-  pointillismEffect();
+  pixelDataManipulation();
 }
 
-function pointillismEffect() {
-  let pointSize = 5; // Adjust this value for different effects
-  
+function pixelDataManipulation() {
   img.loadPixels();
-  for (let y = 0; y < img.height; y += pointSize) {
-    for (let x = 0; x < img.width; x += pointSize) {
+  for (let y = 0; y < img.height; y++) {
+    for (let x = 0; x < img.width; x++) {
       let index = (x + y * img.width) * 4;
       let r = img.pixels[index];
       let g = img.pixels[index + 1];
       let b = img.pixels[index + 2];
       
-      fill(r, g, b);
-      noStroke();
-      ellipse(x, y, pointSize, pointSize);
+      // Invert colors
+      r = 255 - r;
+      g = 255 - g;
+      b = 255 - b;
+      
+      img.pixels[index] = r;
+      img.pixels[index + 1] = g;
+      img.pixels[index + 2] = b;
     }
   }
+  img.updatePixels();
+  image(img, 0, 0);
 }
